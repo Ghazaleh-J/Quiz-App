069072d60d27b61307326a13dfa9fba0d05d69f1
Ghazaeh-J <ghazal.javadi@gmail.com>
2022-04-15 00:49:55 -0700

    Added Questions

---
+++ script.js
@@ -0,0 +1,31 @@
+var intro = document.querySelector(".Intro");
+var questionEl = document.querySelector(".question-container");
+
+
+
+var questionList = [ {
+    qTitle: "What is the capital of Netherlands?",
+    choices: ["Arnhem", "Amsterdam", "Rotterdam", "Utrecht"],
+    correct: "Amsterdam"
+}, {
+    qTitle: "Which country is NOT in Europe?",
+    choices: ["Belgium", "Slovenia", "Denmark", "Morocco"],
+    correct: "Morocco",
+}, {
+    qTitle: "What is the capital of Australia?",
+    choices: ["Sydney", "Brisbane", "Melbourne", "Canberra"],
+   correct:"Canberra",
+}, {
+    qTitle: "Which country has the longest coastline?",
+    choices: ["Australia", "Canada", "China", "Netherlands"],
+    correct: "Canada"
+}, {
+    qTitle: "Which US state has more people than all of Canada?",
+    choices: ["Georgia", "Pennsylvania", "California", "Washington"],
+    correct: "California",
+}
+];
+
+
+
+    

6647b5f95ebd23e826bdf01f1c1ab2ba983e868e
Ghazaeh-J <ghazal.javadi@gmail.com>
2022-04-16 01:46:19 -0700

    timer done

--- script.js
+++ script.js
@@ -2,15 +2,35 @@ var intro = document.querySelector("#Intro");
 var questionContainer = document.querySelector("#question-container");
 var questionEl = document.querySelector("#question");
 var answerContainer = document.querySelector("#answers");
+var timerr = document.querySelector("#timer");
+// var finalContainer = document.querySelector("#lastScreen");
+// var done = document.querySelector("#allDone");
+// var score = document.querySelector("#finalScore");
+var timeLeft = 60;
 var curentQuestionIndex = 0;
+var userChoice = [];
+
+
+
+
+function counter (){
+
+    var timer = setInterval(function(){
+        timeLeft--;
+        timerr.innerText = timeLeft;
+          if (timeLeft === 0) {
+            clearInterval(timer);
+        }
+    },1000);    
+}
+
 
 var button = document.getElementById("start");
 button.addEventListener("click", function(){
     intro.setAttribute("style", "display:none");
     showQuestion();
-})
-
-
+    counter();
+});
 
 
 
@@ -38,11 +58,15 @@ var questionList = [ {
 ];
 
 
+
+
+
+
+
 function showQuestion () {
     questionEl.innerText = questionList[curentQuestionIndex].qTitle;
     var choices = questionList[curentQuestionIndex].choices;
     answerContainer.innerHTML = ""
-
     for (var i = 0; i < 4; i++) {
         var choiceButton = document.createElement("button");
         choiceButton.innerText = choices[i];
@@ -52,24 +76,48 @@ function showQuestion () {
             correctAnswer(event.target.innerText);
             curentQuestionIndex++;
             showQuestion();
-
-
         } )
-
     }
-
-}
-// if statement for correct answer/ user choice with the current question correct answer
-
-function correctAnswer (userChoice){
-    // if else statement
-
 }
 
-// function for what happen if the quiz ends (initial and score)
-// function for adding an array of initial high score to local storage
-//function for the timer / time left var global  -5 for wrong answer
-// function similar for loop to display high scores
-// local storage having an array of object to add to your local storage
 
+function correctAnswer (){
+    if (userChoice !== questionList[curentQuestionIndex].correct){
+        timeLeft-=5;
+    }
+};
+
+
+// function for what happen if the quiz ends (initial and score)  AllDone  "NOT WORKING"
+// function lastScreen (){
+//     if (curentQuestionIndex[4] || timeLeft === 0) {
+       
+//     }
     
+
+// }
+// NOT WORKING
+// function saveHighScore(){
+//     var initial = initialEl.ariaValueMax.trim();
+//     if (initial !== ""){
+//       var highScore = JSON.parse(window.localStorage.getItem("highScore")) || [];
+//       var newScore = {
+//           score : timeLeft,
+//           initial : initial
+//       };
+//       highScore.push(newScore);
+//       window.localStorage.setItem("highScore", JSON.stringify(highScore));
+
+//     }
+// }
+
+
+
+
+
+// function similar to for loop to display high scores
+
+// local storage having an array of object to add to your local storage
+// function for adding an array of initial high score to local storage
+
+   

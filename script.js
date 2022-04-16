4e14c58fbe417b6d4d24726203c65cb792e9cd2d
Ghazaeh-J <ghazal.javadi@gmail.com>
2022-04-15 17:40:19 -0700

    Added functions

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-var intro = document.querySelector(".Intro");
-var questionEl = document.querySelector(".question-container");
+var intro = document.querySelector("#Intro");
+var questionContainer = document.querySelector("#question-container");
+var questionEl = document.querySelector("#question");
+var answerContainer = document.querySelector("#answers");
+var curentQuestionIndex = 0;
+
+var button = document.getElementById("start");
+button.addEventListener("click", function(){
+    intro.setAttribute("style", "display:none");
+    showQuestion();
+})
+
+
 
 
 
@@ -27,5 +38,38 @@ var questionList = [ {
 ];
 
 
+function showQuestion () {
+    questionEl.innerText = questionList[curentQuestionIndex].qTitle;
+    var choices = questionList[curentQuestionIndex].choices;
+    answerContainer.innerHTML = ""
+
+    for (var i = 0; i < 4; i++) {
+        var choiceButton = document.createElement("button");
+        choiceButton.innerText = choices[i];
+        answerContainer.append(choiceButton);
+        choiceButton.addEventListener("click", function(event){
+            event.preventDefault();
+            correctAnswer(event.target.innerText);
+            curentQuestionIndex++;
+            showQuestion();
+
+
+        } )
+
+    }
+
+}
+// if statement for correct answer/ user choice with the current question correct answer
+
+function correctAnswer (userChoice){
+    // if else statement
+
+}
+
+// function for what happen if the quiz ends (initial and score)
+// function for adding an array of initial high score to local storage
+//function for the timer / time left var global  -5 for wrong answer
+// function similar for loop to display high scores
+// local storage having an array of object to add to your local storage
 
     
